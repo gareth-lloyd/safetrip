98879f8ae3974f0f3cc1c6f48423c7f724396318
Gareth Lloyd <glloyd@gmail.com>
2011-06-18 15:21:55 +0100

    moved media to static

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import direct_to_template
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 from safeweb import urls as safeweb_urls
@@ -11,3 +12,5 @@ urlpatterns = patterns('',
     url(r'^', include(safeweb_urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
